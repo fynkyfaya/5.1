909c3eca2170bf7a19e14130aaf648d87cb39858
Фадеев Тимур Сергеевич <110234293+fynkyfaya@users.noreply.github.com>
2022-12-25 20:09:21 +0200

    Add files via upload

---
+++ Program (16).cs
@@ -0,0 +1,14 @@
+﻿int Sing(int a)
+{
+    if (a > 0)
+        return 1;
+    if (a < 0)
+        return -1;
+    return 0;
+}
+
+int x = int.Parse(Console.ReadLine());
+int y = int.Parse(Console.ReadLine());
+
+int z = Sing(x) + Sing(y);
+Console.WriteLine(z);
